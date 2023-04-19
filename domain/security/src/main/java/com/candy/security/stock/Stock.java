b23660054a3628262b2e624ddcaf84b226d44a1b
戚中校 <qizhongxiao@yeetech.com>
2023-04-19 17:58:32 +0800

    init

---
+++ domain/security/src/main/java/com/candy/security/stock/Stock.java
@@ -0,0 +1,32 @@
+package com.candy.security.stock;
+
+import lombok.Data;
+
+@Data
+public class Stock {
+
+    private String code;
+
+    private String symbol;
+
+    private String name;
+
+    private String fullName;
+
+    private String area;
+
+    private String industry;
+
+    private String market;
+
+    private String exchange;
+
+    private String listDate;
+
+    private String deListDate;
+
+
+
+
+
+}
---
+++ domain/security/src/main/java/com/candy/security/stock/StockRepository.java
@@ -0,0 +1,7 @@
+package com.candy.security.stock;
+
+public interface StockRepository {
+
+    Stock getStockByKey(String key);
+
+}
